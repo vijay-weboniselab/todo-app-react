838cbdb3a4655cbdeb4144e37a177e00ea6a7a1e
vijay-weboniselab <vijay.kamble@weboniselab.com>
2020-11-09 18:37:13 +0530

    custom input

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,9 +1,19 @@
-import React from 'react'
 
-export default function LoginPage() {
+import React from 'react';
+import Input from '../SharedComponents/Input/Input';
+import classes from './LoginPage.module.css';
+
+const LoginPage = () =>{
+    
     return (
-        <div>
-            login page
+        <div className={classes.loginContainer}>
+            <form>
+                <Input type="text" name="username" id="username" label="Username"/>
+                <Input type="password" name="password" id="password" label="Password"/>
+               
+            </form>
         </div>
     )
 }
+
+export default LoginPage;
