c5b31aa645e4657bf02e5515b6e44f8cc9191690
vijay-weboniselab <vijay.kamble@weboniselab.com>
2020-11-15 15:31:28 +0530

    Updates

--- src/Charts/Histogram/Histogram.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Histogram() {
-    return (
-        <div>
-            <h1>Histogram will be here soon ;) </h1>
-        </div>
-    )
-}
--- src/TasksPage/TaskDetails/TaskDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TaskDetails() {
-    return (
-        <div>
-            <h3>task details</h3>
-        </div>
-    )
-}
